4d46a8df035010ecdf3705f1671aa7b0ec2cadeb
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-07-25 04:45:03 -0700

    1000-sort_deck.c

--- deck.h
+++ deck.h
@@ -1,12 +1,14 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include <stdlib.h>
+
 /**
- * enum kind_e - The card 
- * @SPADE: The spade 
- * @HEART: The heart 
- * @CLUB: The club 
- * @DIAMOND: The diamond
+ * enum kind_e - Enumeration of card suits.
+ * @SPADE: Spades suit.
+ * @HEART: Hearts suit.
+ * @CLUB: Clubs suit.
+ * @DIAMOND: Diamonds suit.
  */
 typedef enum kind_e
 {
@@ -17,20 +19,20 @@ typedef enum kind_e
 } kind_t;
 
 /**
- * struct card_s - card
+ * struct card_s - Playing card
  *
- * @val: val of the card
+ * @value: Value of the card
  * From "Ace" to "King"
  * @kind: Kind of the card
  */
 typedef struct card_s
 {
-	const char *val;
+	const char *value;
 	const kind_t kind;
 } card_t;
 
 /**
- * struct deck_node_s - the card deck
+ * struct deck_node_s - Deck of card
  *
  * @card: Pointer to the card of the node
  * @prev: Pointer to the previous node of the list
@@ -44,4 +46,5 @@ typedef struct deck_node_s
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
+
 #endif
